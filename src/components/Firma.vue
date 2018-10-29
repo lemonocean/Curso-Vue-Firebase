46b5d7ee7c1d6b039b9bef44858fb8862930a897
jormabs <jormabs@hotmail.com>
2018-10-29 08:57:45 -0500

    S03 - 13 mousemove

--- src/components/Firma.vue
+++ src/components/Firma.vue
@@ -1,12 +1,35 @@
 <template>
-  <canvas ref="canFirma" class="firma" width="380" height="200"></canvas>
+  <canvas @mousemove="dibujar" ref="canFirma" class="firma" width="380" height="200"></canvas>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      ctx: null
+      ctx: null,
+      x0: -1,
+      y0: -1
+    }
+  },
+  methods: {
+    dibujar(event) {
+
+      if(this.x0 == -1 || this.y0 == -1) {
+        this.x0 = event.offsetX
+        this.y0 = event.offsetY
+        return
+      }
+
+      const x1 = event.offsetX
+      const y1 = event.offsetY
+
+      this.ctx.beginPath()
+      this.ctx.moveTo(this.x0, this.y0)
+      this.ctx.lineTo(x1, y1)
+      this.ctx.stroke()
+
+      this.x0 = x1
+      this.y0 = y1
     }
   },
   mounted() {
