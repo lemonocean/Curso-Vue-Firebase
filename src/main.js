7f0e5a145e17664dda390d74cbcbdde3bd1b8b55
jormabs <jormabs@hotmail.com>
2018-11-05 17:23:09 -0500

    S05 - 02 Instalación

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import './plugins/vuetify'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App)
