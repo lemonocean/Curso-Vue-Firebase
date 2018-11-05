523ef9dd6c9b1dbadecb9e983238f0c646723897
jormabs <jormabs@hotmail.com>
2018-11-05 05:11:55 -0500

    S04 - 09 Menú

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="menu" app temporary>
+      <v-list>
+        <v-list-tile @click="seleccionar('home')">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Inicio'"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('perfil')">
+          <v-list-tile-action>
+            <v-icon>account_circle</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Perfil'"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('registro')">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Registro'"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="seleccionar('login')">
+          <v-list-tile-action>
+            <v-icon>arrow_forward</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="'Ingresar'"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar color="primary" dark app>
+      <v-toolbar-side-icon @click="menu = !menu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>{{ titulo }}</span>
       </v-toolbar-title>      
@@ -27,7 +64,14 @@ export default {
   data () {
     return {
       titulo: 'Súper Ópera',
-      componenteActual: 'home'
+      componenteActual: 'home',
+      menu: false
+    }
+  },
+  methods: {
+    seleccionar(nombre) {
+      this.componenteActual = nombre
+      this.menu = false
     }
   }
 }
