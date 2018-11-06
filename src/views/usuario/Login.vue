66d35e183a7f0e95bca9a53b1c2a6463e8313861
jormabs <jormabs@hotmail.com>
2018-11-06 06:34:44 -0500

    S05 - 05 Validación Login: Password

--- src/views/usuario/Login.vue
+++ src/views/usuario/Login.vue
@@ -9,7 +9,7 @@
         </v-toolbar>
         <v-card-text>
           <v-text-field label="Email" v-model="email" :error-messages="erroresEmail" @blur="$v.email.$touch()"></v-text-field>
-          <v-text-field label="Password" type="password"></v-text-field>
+          <v-text-field label="Password" v-model="password" :error-messages="erroresPassword" @blur="$v.password.$touch()" type="password"></v-text-field>
         </v-card-text>
         <v-card-text>
           <v-layout justify-end>
@@ -23,12 +23,24 @@
 
 <script>
 
-import { required, email } from 'vuelidate/lib/validators'
+import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 
 export default {
   data() {
     return {
-      email: ''
+      email: '',
+      password: ''
+    }
+  },  
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(6),
+      maxLength: maxLength(20)
     }
   },
   computed: {
@@ -38,12 +50,14 @@ export default {
       if(!this.$v.email.required) { errores.push('Ingresa tu email.') }
       if(!this.$v.email.email) { errores.push('Ingresa un email válido.') }
       return errores
-    }
-  },
-  validations: {
-    email: {
-      required,
-      email
+    },
+    erroresPassword() {
+      let errores = []
+      if(!this.$v.password.$dirty) { return errores }
+      if(!this.$v.password.required) { errores.push('Ingresa tu password.') }
+      if(!this.$v.password.minLength) { errores.push('Ingresa al menos 6 caracteres.') }
+      if(!this.$v.password.maxLength) { errores.push('Ingresa máximo 20 caracteres.') }
+      return errores
     }
   }
 }
