524c3f18c7662fecaf285601d87e7db5f257ede3
jormabs <jormabs@hotmail.com>
2018-11-06 11:20:05 -0500

    S05 - 06 Validación Login: Formulario

--- src/views/usuario/Login.vue
+++ src/views/usuario/Login.vue
@@ -8,12 +8,12 @@
           </v-toolbar-title>
         </v-toolbar>
         <v-card-text>
-          <v-text-field label="Email" v-model="email" :error-messages="erroresEmail" @blur="$v.email.$touch()"></v-text-field>
-          <v-text-field label="Password" v-model="password" :error-messages="erroresPassword" @blur="$v.password.$touch()" type="password"></v-text-field>
+          <v-text-field label="Email" v-model="formulario.email" :error-messages="erroresEmail" @blur="$v.formulario.email.$touch()"></v-text-field>
+          <v-text-field label="Password" @keyup.enter="ingresar" v-model="formulario.password" :error-messages="erroresPassword" @blur="$v.formulario.password.$touch()" type="password"></v-text-field>
         </v-card-text>
         <v-card-text>
           <v-layout justify-end>
-            <v-btn @click="vista++" color="secondary">Siguiente</v-btn>
+            <v-btn @click="ingresar" :depressed="$v.formulario.$invalid" :disabled="$v.formulario.$invalid" color="secondary">Ingresar</v-btn>
           </v-layout>
         </v-card-text>
       </v-card>
@@ -28,35 +28,49 @@ import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 export default {
   data() {
     return {
-      email: '',
-      password: ''
+      formulario: {
+        email: '',
+        password: ''
+      }
     }
-  },  
+  },
   validations: {
-    email: {
-      required,
-      email
-    },
-    password: {
-      required,
-      minLength: minLength(6),
-      maxLength: maxLength(20)
+    formulario: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(20)
+      }
+    }
+  },
+  methods: {
+    ingresar() {
+      if(this.$v.formulario.$invalid) { 
+        this.$v.formulario.$touch()
+        return 
+      }
+
+      alert('Consultando...')
     }
   },
   computed: {
     erroresEmail() {
       let errores = []
-      if(!this.$v.email.$dirty) { return errores }
-      if(!this.$v.email.required) { errores.push('Ingresa tu email.') }
-      if(!this.$v.email.email) { errores.push('Ingresa un email válido.') }
+      if (!this.$v.formulario.email.$dirty) { return errores }
+      if (!this.$v.formulario.email.required) { errores.push('Ingresa tu email.') }
+      if (!this.$v.formulario.email.email) { errores.push('Ingresa un email válido.') }
       return errores
     },
     erroresPassword() {
       let errores = []
-      if(!this.$v.password.$dirty) { return errores }
-      if(!this.$v.password.required) { errores.push('Ingresa tu password.') }
-      if(!this.$v.password.minLength) { errores.push('Ingresa al menos 6 caracteres.') }
-      if(!this.$v.password.maxLength) { errores.push('Ingresa máximo 20 caracteres.') }
+      if (!this.$v.formulario.password.$dirty) { return errores }
+      if (!this.$v.formulario.password.required) { errores.push('Ingresa tu password.') }
+      if (!this.$v.formulario.password.minLength) { errores.push('Ingresa al menos 6 caracteres.') }
+      if (!this.$v.formulario.password.maxLength) { errores.push('Ingresa máximo 20 caracteres.') }
       return errores
     }
   }
