4932efd30eefc32a3664941216dc93916946401c
jormabs <jormabs@hotmail.com>
2018-11-20 09:51:35 -0500

    S10 - 14 Ingresando con Facebook - Login

--- src/views/usuario/Login.vue
+++ src/views/usuario/Login.vue
@@ -7,13 +7,29 @@
             Ingresa tus Credenciales
           </v-toolbar-title>
         </v-toolbar>
+        <v-card-text class="subheading text-xs-center">
+            Ingresa con tu red social preferida
+          </v-card-text>
+          <v-card-text>
+            <v-layout justify-center>
+              <a @click="ingresar('facebook')">
+                <v-avatar tile>
+                  <img  alt="Ingreso Facebook" src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M483.7 0H28.3C12.7 0 0 12.7 0 28.3v455.5C0 499.3 12.7 512 28.3 512h245.2V314H207v-77.5h66.5v-57c0-66.1 40.4-102.1 99.4-102.1 28.3 0 52.5 2.1 59.6 3v69.1h-40.7c-32.1 0-38.3 15.3-38.3 37.6v49.4h76.7l-10 77.5h-66.7v198h130.2c15.6 0 28.3-12.7 28.3-28.3V28.3C512 12.7 499.3 0 483.7 0z' fill='%234267b2'/><path d='M353.5 512V314h66.8l10-77.5h-76.8v-49.4c0-22.4 6.2-37.6 38.3-37.6h40.7V80.4c-7.1-.9-31.4-3-59.6-3-59 0-99.4 36-99.4 102.1v57H207V314h66.5v198h80z' fill='%23fff'/></svg>">
+                </v-avatar>
+              </a>
+            </v-layout>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-text class="subheading text-xs-center">
+            Ingresa con una cuenta de email y constraseña
+          </v-card-text>
         <v-card-text>
           <v-text-field label="Email" autofocus v-model="formulario.email" :error-messages="erroresEmail" @blur="$v.formulario.email.$touch()"></v-text-field>
-          <v-text-field label="Password" @keyup.enter="ingresar" v-model="formulario.password" :error-messages="erroresPassword" @blur="$v.formulario.password.$touch()" type="password"></v-text-field>
+          <v-text-field label="Password" @keyup.enter="ingresar('email')" v-model="formulario.password" :error-messages="erroresPassword" @blur="$v.formulario.password.$touch()" type="password"></v-text-field>
         </v-card-text>
         <v-card-text>
           <v-layout justify-end>
-            <v-btn @click="ingresar" :depressed="$v.formulario.$invalid" :disabled="$v.formulario.$invalid" color="secondary">Ingresar</v-btn>
+            <v-btn @click="ingresar('email')" :depressed="$v.formulario.$invalid" :disabled="$v.formulario.$invalid" color="secondary">Ingresar</v-btn>
           </v-layout>
         </v-card-text>
         <v-card-actions>
@@ -64,7 +80,7 @@
 
 import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 import { mapMutations, mapGetters } from 'vuex'
-import { auth } from '@/firebase'
+import { firebase, auth } from '@/firebase'
 
 export default {
   data() {
@@ -97,7 +113,18 @@ export default {
   methods: {
     ...mapMutations(['mostrarOcupado', 'ocultarOcupado', 'mostrarExito', 'mostrarAdvertencia']),
     ...mapMutations('sesion', ['actualizarUsuario']),
-    async ingresar() {
+    ingresar(metodo) {
+      switch(metodo) {
+        case 'email':
+          this.ingresarEmail()
+          break
+
+        case 'facebook':
+          this.ingresarFacebook()
+          break
+      }
+    },
+    async ingresarEmail() {
       if (this.$v.formulario.$invalid) {
         this.$v.formulario.$touch()
         return
@@ -131,6 +158,25 @@ export default {
       }
       finally {
         this.ocultarOcupado()
+      }
+    },
+    async ingresarFacebook() {
+      let provider = new firebase.auth.FacebookAuthProvider()
+
+      provider.setCustomParameters({
+        'display': 'popup'
+      })
+
+      auth.languageCode = 'es_CO'
+
+      try {
+        await auth.signInWithPopup(provider)
+
+        this.mostrarExito(this.saludo)
+        this.$router.push({ name: 'home' })
+      }
+      catch(error) {
+        this.mostrarError('Ocurrió un error validando tu información.')
       }
     },
     async enviarSolicitudPassword() {
