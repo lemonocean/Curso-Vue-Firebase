b026a64d5c12a1bcb76bcce7e8fbaa47ea393fbb
jormabs <jormabs@hotmail.com>
2018-11-20 08:39:11 -0500

    S10 - 13 Ingresando con Facebook - Registro

--- src/views/usuario/Registro.vue
+++ src/views/usuario/Registro.vue
@@ -8,14 +8,30 @@
               Regístrate
             </v-toolbar-title>
           </v-toolbar>
+          <v-card-text class="subheading text-xs-center">
+            Regístrate con tu red social preferida
+          </v-card-text>
+          <v-card-text>
+            <v-layout justify-center>
+              <a @click="siguiente(1, 'facebook')">
+                <v-avatar tile>
+                  <img  alt="Ingreso Facebook" src="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M483.7 0H28.3C12.7 0 0 12.7 0 28.3v455.5C0 499.3 12.7 512 28.3 512h245.2V314H207v-77.5h66.5v-57c0-66.1 40.4-102.1 99.4-102.1 28.3 0 52.5 2.1 59.6 3v69.1h-40.7c-32.1 0-38.3 15.3-38.3 37.6v49.4h76.7l-10 77.5h-66.7v198h130.2c15.6 0 28.3-12.7 28.3-28.3V28.3C512 12.7 499.3 0 483.7 0z' fill='%234267b2'/><path d='M353.5 512V314h66.8l10-77.5h-76.8v-49.4c0-22.4 6.2-37.6 38.3-37.6h40.7V80.4c-7.1-.9-31.4-3-59.6-3-59 0-99.4 36-99.4 102.1v57H207V314h66.5v198h80z' fill='%23fff'/></svg>">
+                </v-avatar>
+              </a>
+            </v-layout>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-text class="subheading text-xs-center">
+            Regístrate con una cuenta de email y constraseña
+          </v-card-text>
           <v-card-text>
             <v-text-field @blur="$v.f1.email.$touch()" autofocus :error-messages="erroresEmail" v-model="f1.email" label="Email"></v-text-field>
             <v-text-field @blur="$v.f1.password.$touch()" :error-messages="erroresPassword" v-model="f1.password" label="Password" type="password"></v-text-field>
-            <v-text-field @keyup.enter="siguiente(1)" @blur="$v.f1.repetirPassword.$touch()" :error-messages="erroresRepetirPassword" v-model="f1.repetirPassword" label="Repetir Password" type="password"></v-text-field>
+            <v-text-field @keyup.enter="siguiente(1, 'email')" @blur="$v.f1.repetirPassword.$touch()" :error-messages="erroresRepetirPassword" v-model="f1.repetirPassword" label="Repetir Password" type="password"></v-text-field>
           </v-card-text>
           <v-card-text>
             <v-layout justify-end>
-              <v-btn :depressed="$v.f1.$invalid" :disabled="$v.f1.$invalid" @click="siguiente(1)" color="secondary">Siguiente</v-btn>
+              <v-btn :depressed="$v.f1.$invalid" :disabled="$v.f1.$invalid" @click="siguiente(1, 'email')" color="secondary">Siguiente</v-btn>
             </v-layout>
           </v-card-text>
           <v-card-actions>
@@ -84,11 +100,12 @@
 import { required, email, minLength, maxLength, sameAs } from 'vuelidate/lib/validators'
 import { nombreCompuesto } from '@/utilidades/validaciones'
 import { mapMutations, mapGetters } from 'vuex'
-import { auth } from '@/firebase'
+import { firebase, auth } from '@/firebase'
 
 export default {
   data() {
     return {
+      metodo: 'email',
       vista: 1,
       f1: {
         email: '',
@@ -145,14 +162,15 @@ export default {
   methods: {
     ...mapMutations(['mostrarOcupado', 'ocultarOcupado', 'mostrarExito', 'mostrarError', 'mostrarAdvertencia']),
     ...mapMutations('sesion', ['actualizarUsuario']),
-    siguiente(vista) {
+    siguiente(vista, metodo) {
       switch (vista) {
         case 1:
-          if (this.$v.f1.$invalid) {
+          if (this.$v.f1.$invalid && metodo == 'email') {
             this.$v.f1.$touch()
             return
           }
           else {
+            this.metodo = metodo
             this.vista++
           }
           break
@@ -170,6 +188,17 @@ export default {
     async registrar() {
       if (this.$v.fechaNacimiento.$invalid) { return }
 
+      switch(this.metodo) {
+        case 'email':
+          this.registrarEmail()
+          break
+
+        case 'facebook':
+          this.registrarFacebook()
+          break
+      }
+    },
+    async registrarEmail() {
       try {
         this.mostrarOcupado({ titulo: 'Creando Registro', mensaje: 'Estamos registrando tu información...' })
 
@@ -193,6 +222,25 @@ export default {
       }
       finally {
         this.ocultarOcupado()
+      }
+    },
+    async registrarFacebook() {
+      let provider = new firebase.auth.FacebookAuthProvider()
+
+      provider.setCustomParameters({
+        'display': 'popup'
+      })
+
+      auth.languageCode = 'es_CO'
+
+      try {
+        await auth.signInWithPopup(provider)
+
+        this.mostrarExito(this.saludo)
+        this.$router.push({ name: 'home' })
+      }
+      catch(error) {
+        this.mostrarError('Ocurrió un error registrando tu cuenta.')
       }
     },
     enter() {
