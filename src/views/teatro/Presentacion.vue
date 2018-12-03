93c4f88465f4a8a1cd93d8b3628a9e7d4e396cfe
jormabs <jormabs@hotmail.com>
2018-12-03 16:32:06 -0500

    S11 - 22 Reservas - Presentación - Información Asientos

--- src/views/teatro/Presentacion.vue
+++ src/views/teatro/Presentacion.vue
@@ -1,9 +1,18 @@
 <template>
   <v-layout justify-center align-start>
-    <v-layout column justify-center text-xs-center my-3 class="secondary--text">
-      <h3 class="display-2">{{ obra.titulo }}</h3>
-      <h4 class="display-1">{{ teatro.nombre }}</h4>
-      <h5 class="headline">{{ fechaPresentacion }}</h5>
+    <v-layout v-if="presentacion && asientos" column>
+      <v-layout column justify-center text-xs-center my-3 class="secondary--text">
+        <h3 class="display-2">{{ obra.titulo }}</h3>
+        <h4 class="display-1">{{ teatro.nombre }}</h4>
+        <h5 class="headline">{{ fechaPresentacion }}</h5>
+      </v-layout>
+      <v-divider></v-divider>
+      <v-layout column justify-center my-3>
+        <v-card v-for="asiento in asientos" :key="asiento.aid">
+          <v-icon :color="asiento.color">lens</v-icon>
+          <span>{{ asiento.descripcion + ': $ ' + asiento.precio }}</span>
+        </v-card>
+      </v-layout>
     </v-layout>
   </v-layout>
 </template>
@@ -19,7 +28,8 @@ export default {
     return {
       teatro: null,
       obra: null,
-      presentacion: null
+      presentacion: null,
+      asientos: null
     }
   },
   computed: {
@@ -48,8 +58,8 @@ export default {
 
       try {
         let teatroDoc = await db.collection('teatros')
-                                .doc(tid)
-                                .get()
+          .doc(tid)
+          .get()
 
         if (teatroDoc.exists) {
           this.teatro = teatroDoc.data()
@@ -60,8 +70,8 @@ export default {
         }
 
         let obraDoc = await db.collection('obras')
-                              .doc(oid)
-                              .get()
+          .doc(oid)
+          .get()
 
         if (obraDoc.exists) {
           this.obra = obraDoc.data()
@@ -72,10 +82,10 @@ export default {
         }
 
         let presentacionDoc = await db.collection('obras')
-                                      .doc(oid)
-                                      .collection('presentaciones')
-                                      .doc(pid)
-                                      .get()
+          .doc(oid)
+          .collection('presentaciones')
+          .doc(pid)
+          .get()
 
         if (presentacionDoc.exists) {
           this.presentacion = presentacionDoc.data()
@@ -84,6 +94,21 @@ export default {
           this.$router.push({ name: '404' })
           return
         }
+
+        this.asientos = this.teatro.asientos.map(asiento => {
+          let precio = this.obra.categorias.find(item => item.categoria == asiento.categoria).precio
+          let color = this.teatro.categorias.find(item => item.categoria == asiento.categoria).color
+
+          return {
+            aid: asiento.x + '-' + asiento.y,
+            categoria: asiento.categoria,
+            descripcion: asiento.descripcion,
+            x: asiento.x,
+            y: asiento.y,
+            precio,
+            color
+          }
+        })
       }
       catch (error) {
         this.mostrarError('Ocurrió un error consultando la información de la presentación.')
