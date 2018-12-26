7c63343e7cd7293cde01065810fae44006e78a61
jormabs <jormabs@hotmail.com>
2018-12-26 18:36:19 -0500

    S13 - 03 Creando la Función

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+admin.firestore().settings({
+  timestampsInSnapshots: true
+})
+const db = admin.firestore()
+
+exports.actualizarNombresApellidos = functions.firestore
+  .document('usuarios/{uid}')
+  .onUpdate(async (change, contex) => {
+    const actual = change.after.data()
+    const anterior = change.before.data()
+
+    if (actual.nombres !== anterior.nombres || actual.apellidos !== anterior.apellidos) {
+      let batch = db.batch()
+
+      try {
+        let resultado = await db.collection('usuarios')
+                                .doc(contex.params.uid)
+                                .collection('reservas')
+                                .get()
+
+        resultado.forEach(doc => {
+
+          let reserva = doc.data()
+
+          batch.update(
+            db.collection('obras')
+              .doc(reserva.presentacion.obra.oid)
+              .collection('presentaciones')
+              .doc(reserva.presentacion.pid)
+              .collection('reservas')
+              .doc(reserva.rid),
+              { 'usuario.nombres': actual.nombres, 'usuario.apellidos': actual.apellidos }
+          )
+        })
+
+        return batch.commit()
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    return null
+  })
