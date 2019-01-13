1d5f807d5dae76b2314e89d5d0a6fe6472ad3a6c
jormabs <jormabs@hotmail.com>
2019-01-13 08:47:46 -0500

    S13 - 08 Miniaturas - Creando la Funcion

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,75 @@ admin.firestore().settings({
   timestampsInSnapshots: true
 })
 const db = admin.firestore()
+const auth = admin.auth()
+const storage = admin.storage()
+
+const tmpdir = require('os').tmpdir
+const join = require('path').join
+const fs = require('fs-extra')
+const sharp = require('sharp')
+const dimensiones = [512, 256, 128, 64, 32]
+
+exports.generarMiniaturas = functions.https.onCall(async (data, context) => {
+  let token = await auth.verifyIdToken(data.idToken)
+
+  if (!token) {
+    console.log('Llamado sin autorización.')
+    return false
+  }
+
+  let uid = token.uid
+  let fotoId = data.fotoId
+
+  const bucket = storage.bucket()
+
+  let imagenOriginal = `usuarios/${uid}/fotos-perfil/${fotoId}.jpg`
+  let directorioFisico = join(tmpdir(), 'temporal')
+  let rutaFisica = join(directorioFisico, fotoId + '.jpg')
+
+  try {
+    await fs.ensureDir(directorioFisico)
+
+    await bucket.file(imagenOriginal).download({
+      destination: rutaFisica
+    })
+
+    let promises = dimensiones.map(async dimension => {
+      let ancho = dimension
+      let alto = dimension
+
+      const imagenMiniatura = `${fotoId}-${ancho}x${alto}.jpg`
+      const rutaFisicaMiniatura = join(directorioFisico, imagenMiniatura)
+
+      await sharp(rutaFisica)
+        .resize(ancho, alto)
+        .toFile(rutaFisicaMiniatura)
+
+      const destino = `usuarios/${uid}/fotos-perfil/${imagenMiniatura}`
+
+      await bucket.upload(rutaFisicaMiniatura, {
+        destination: destino,
+        uploadType: 'media',
+        metadata: {
+          contentType: 'image/jpg'
+        }
+      })
+
+      await fs.remove(rutaFisicaMiniatura)
+    })
+
+    await Promise.all(promises)
+
+    await fs.remove(rutaFisica)
+
+    await bucket.file(imagenOriginal).delete()
+
+    return true
+  } catch (error) {
+    console.log(error)
+    return false
+  }
+})
 
 exports.actualizarNombresApellidos = functions.firestore
   .document('usuarios/{uid}')
